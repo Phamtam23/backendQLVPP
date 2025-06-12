776ecbc9cdbe4698a8339fc339b305f23251e8f6
Le Dai Thanh <thanh261220@gmail.com>
2025-06-12 15:03:32 +0700

    fix conflict

--- src/router/api.js
+++ src/router/api.js
@@ -3,8 +3,7 @@ const routerAPI = express.Router();
 const { change_role, get_role, create_user, get_user, edit_user, delete_user, handlogin, get_account } = require('../controller/user_controller');
 const { get_dskehoach, get_chitietkehoach } = require('../controller/kehoach_controller');
 const { create_yeucau } = require('../controller/guiyeucau_controller');
-<<<<<<< HEAD
-const { get_thietbi, get_phong } = require('../controller/thietbi_controller');
+const { get_thietbi, get_phong, get_danhmuc } = require('../controller/thietbi_controller');
 const { get_nhathau, get_nhathauById } = require('../controller/nhathau_controller');
 const { get_hopdong, get_hopdongById } = require('../controller/hopdong_controller');
 const { get_linhvuc } = require('../controller/linhvuc_controller');
@@ -12,12 +11,9 @@ const { getDashboardData } = require('../controller/dashboard_controller');
 //const auth=require('../midderwaler/auth')
 //const delay=require('../midderwaler/delay')
 //routerAPI.all("*",auth)
-=======
-const { get_thietbi, get_phong,get_danhmuc } = require('../controller/thietbi_controller');
 
 // Import upload middleware
 const upload = require('../middleware/upload');
->>>>>>> master
 
 // Xử lý đăng nhập
 routerAPI.post('/login', handlogin);
